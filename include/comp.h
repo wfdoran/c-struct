e10e04868292018060e70772c2f19000bc5550a0
William Doran <wfdoran4@yahoo.com>
2018-05-19 19:24:52 -0700

    comp for chars.

--- include/comp.h
+++ include/comp.h
@@ -37,11 +37,22 @@ int comp_str(char **a, char **b) {
     return strcmp(*a, *b);
 }
 
+int comp_char(char *a, char *b) {
+    if (*a > *b) {
+        return 1;
+    }
+    if (*a < *b) {
+        return -1;
+    }
+    return 0;
+}
+
 #define DEFAULT_COMP(x) _Generic((x), \
     int: &comp_int, \
     int64_t: &comp_int64, \
     float: &comp_float, \
-    char*: comp_str, \
+    char: &comp_char, \
+    char*: &comp_str, \
     default: NULL)
     
 #endif
