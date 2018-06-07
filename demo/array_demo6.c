e2a18c830d8b2ee12626d6a19a98c81d8bbe81c8
William Doran <wfdoran4@yahoo.com>
2018-06-06 21:58:06 -0700

    array_demo6.c

---
+++ demo/array_demo6.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct {int x[5];} intarr_t;
+
+#define data_t intarr_t
+#define prefix intarr
+
+#include <array.h>
+
+#undef data_t
+#undef prefix
+
+
+int main(void) {
+    array_intarr_t a;
+    array_intarr_init(&a);
+    
+    intarr_t b = {.x = {1,2,3,4,5}};
+    array_intarr_append(&a, (intarr_t){.x = {1,2,3,4,5}});
+    return 0;
+}    
