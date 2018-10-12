8f1307963de58150d232fb73108be2f3c5ee72e0
William Doran <wfdoran4@yahoo.com>
2018-10-11 19:17:16 -0700

    updated tree_demo3.c to new interface.

--- demo/tree_demo3.c
+++ demo/tree_demo3.c
@@ -29,23 +29,22 @@ void walk(tnode_double_t *n) {
 int main(void) {
     srand(time(NULL));
     
-    tree_double_t t;
-    tree_double_init(&t);
+    tree_double_t *t = tree_double_init();
     
     int n = 100;
     double vals[n];
     
     for (int i = 0; i < n; i++) {
         vals[i] = ((double) ((rand() >> 3) & 0xffff)) / 0x1000;
-        printf("IN %.4f\n", vals[i]);
-        tree_double_insert(&t, vals[i], NULL);
+        // printf("IN %.4f\n", vals[i]);
+        tree_double_insert(t, vals[i], NULL);
     }
-    printf("Tree Size: %d\n", tree_double_size(&t));
-    printf("Tree Height: %d\n", tree_double_height(&t));
+    printf("Tree Size: %d\n", tree_double_size(t));
+    printf("Tree Height: %d\n", tree_double_height(t));
     printf("\n");
     
     void *state;
-    tree_double_walk_init(&t, &state);
+    tree_double_walk_init(t, &state);
     while (state != NULL) {
 		key_double_value_t pair = tree_double_walk_next(&state);
         printf("%.4f ", pair.key);
@@ -63,21 +62,24 @@ int main(void) {
     //walk(t.root);
     
     for (int i = 0; i < n/2; i++) {
-        printf("OUT %.4f\n", vals[i]);
-        tree_double_delete(&t, vals[i]);
+        // printf("OUT %.4f\n", vals[i]);
+        tree_double_delete(t, vals[i]);
     }
-    
-    // walk(t.root);
-    printf("Tree Size: %d\n", tree_double_size(&t));
-    printf("Tree Height: %d\n", tree_double_height(&t));
     printf("\n");
     
-    tree_double_walk_init(&t, &state);
+    // walk(t.root);
+    printf("Tree Size: %d\n", tree_double_size(t));
+    printf("Tree Height: %d\n", tree_double_height(t));
+    printf("\n");
+    
+    tree_double_walk_init(t, &state);
     while (state != NULL) {
 		key_double_value_t pair = tree_double_walk_next(&state);
         printf("%.4f ", pair.key);
     }
     printf("\n");
+        
+    tree_double_destroy(t);    
         
     return 0;
 }
