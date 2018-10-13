c118ae875abcf169669758d0c38292f95a3ee55f
William Doran <wfdoran4@yahoo.com>
2018-10-13 11:45:51 -0700

    Convert tree_demo5 and tree_demo6 to new interface.

--- demo/tree_demo5.c
+++ demo/tree_demo5.c
@@ -7,16 +7,16 @@
 #undef data_t
 
 int main(void) {
-    tree_str_t t;
-    tree_str_init(&t);
-
-	tree_str_insert(&t, "Ann", "111-222-3333");
-	tree_str_insert(&t, "Bill", "123-456-7890");
-	tree_str_insert(&t, "Charlie", "999-999-9999");
-	tree_str_insert(&t, "Dave", "656-666-6666");
+    tree_str_t *t = tree_str_init();
+    
+	tree_str_insert(t, "Ann", "111-222-3333");
+	tree_str_insert(t, "Bill", "123-456-7890");
+	tree_str_insert(t, "Charlie", "999-999-9999");
+	tree_str_insert(t, "Dave", "656-666-6666");
 	
-	key_str_value_t *rv = tree_str_retrieve(&t, "Bill");
+	key_str_value_t *rv = tree_str_retrieve(t, "Bill");
 	printf("%s\n", rv->value);
 	
+    tree_str_destroy(t);
 	return 0;
 }
---
+++ demo/tree_demo6.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
+
+typedef struct {
+	int x[3];
+} triple_t;
+
+int comp(triple_t *a, triple_t *b) {
+	for (int i = 0; i < 3; i++) {
+		if (a->x[i] < b->x[i]) {
+			return -1;
+		}
+		if (a->x[i] > b->x[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+#define data_t triple_t
+#define prefix trip
+#include <tree.h>
+#undef prefix
+#undef data_t
+
+int main(void) {
+    srand(time(NULL));
+    
+    tree_trip_t *t = tree_trip_init();
+    tree_trip_init(t);
+	tree_trip_set_comp(t, comp);
+    tree_trip_set_value_free(t, free);
+	
+	int n = 10;
+	for (int i = 0; i < n; i++) {
+		triple_t key = {.x[0] = rand(), .x[1] = rand(), .x[2] = rand()};
+		double *value = malloc(sizeof(double));
+		*value = (double) rand() / (1 << 15);
+		tree_trip_insert(t, key, value);
+	}
+	
+    void *state;
+	tree_trip_walk_init(t, &state);
+	while (state != NULL) {
+		key_trip_value_t q = tree_trip_walk_next(&state);
+		printf("%6d %6d %6d  %8.4f \n", q.key.x[0], q.key.x[1], q.key.x[2], *(double*)q.value);
+	}	
+	
+	tree_trip_destroy(t);
+    return 0;
+}
