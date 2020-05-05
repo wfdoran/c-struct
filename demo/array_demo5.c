9d2620addb62ed49cacfecf3c704cc3a4743a07d
William Doran <wfdoran4@yahoo.com>
2020-05-05 06:09:39 -0700

    Revised array_demo5.

--- demo/array_demo5.c
+++ demo/array_demo5.c
@@ -14,19 +14,19 @@
 int main(void) {
     srand(time(NULL));
     
-    array_double_t a;
-    array_double_init(&a);
+    array_double_t *a = array_double_init();
     
     for (int i = 0; i < 10; i++) {
-        array_double_append(&a, ((double) (rand() % 0xfff)) / 0x100);
+        array_double_append(a, ((double) (rand() % 0xfff)) / 0x100);
     }
     
-    array_double_heapify(&a);
+    array_double_heapify(a);
     
-    for (int i = 0; i < array_double_size(&a); i++) {
-        printf("%6.4f \n", array_double_get(&a, i));        
+    for (int i = 0; i < array_double_size(a); i++) {
+        printf("%6.4f \n", array_double_get(a, i));        
     }
-        
+
+    array_double_destroy(&a);
     return 0;
 }
    
