5510b6574fe6fc783e03c7cdfce8bd2780365af7
William Doran <wfdoran4@yahoo.com>
2020-05-29 13:17:43 -0700

    Indent of hash_table.h.

--- include/hash_table.h
+++ include/hash_table.h
@@ -29,23 +29,23 @@
  
 */
 typedef struct HNODE {
-  uint64_t hash;
-  key_t key;
-  value_t value;
+    uint64_t hash;
+    key_t key;
+    value_t value;
 } HNODE;
 
 typedef struct HLIST {
-  int64_t capacity;
-  int64_t size;
-  HNODE **A;
-  uint64_t (*hash_func) (key_t);
-  int (*comp) (key_t, key_t);
-  value_t (*update) (value_t, value_t);
+    int64_t capacity;
+    int64_t size;
+    HNODE **A;
+    uint64_t (*hash_func) (key_t);
+    int (*comp) (key_t, key_t);
+    value_t (*update) (value_t, value_t);
 } HTABLE;
 
 typedef struct HITER {
-  const HTABLE *h;
-  uint64_t curr;
+    const HTABLE *h;
+    uint64_t curr;
 } HITER;
 
 /* 
@@ -62,16 +62,16 @@ typedef struct HITER {
 https://en.wikipedia.org/wiki/Hash_table
 */
 
-static int64_t GLUE3(hash_, prefix, _roundup_pow2) (int64_t x) {
-  x--;
-  x |= x >> 1;
-  x |= x >> 2;
-  x |= x >> 4;
-  x |= x >> 8;
-  x |= x >> 16;
-  x |= x >> 32;
-  x++;
-  return x;
+static int64_t GLUE3 (hash_, prefix, _roundup_pow2) (int64_t x) {
+    x--;
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+    x |= x >> 32;
+    x++;
+    return x;
 }
 
 /* htable_prefix_t* hash_prefix_init(int64_t expected_size); 
@@ -84,27 +84,27 @@ static int64_t GLUE3(hash_, prefix, _roundup_pow2) (int64_t x) {
    - setting the comp function if comp.h recognizes teh key_t type.
 
 */
-HTABLE *GLUE3(hash_, prefix, _init) (int64_t expected_size) {
-  HTABLE* h = malloc(sizeof(HTABLE));
-  if (h == NULL) {
-    return NULL;
-  }
-  
-  h->capacity = expected_size <= 16 ? 16 : GLUE3(hash_, prefix, _roundup_pow2)(expected_size);
-  h->size = 0;
-  h->A = malloc(h->capacity * sizeof(HNODE*));
-  if (h->A == NULL) {
-    return NULL;
-  }
-  for (int64_t i = 0; i < h->capacity; i++) {
-    h->A[i] = NULL;
-  }
-  key_t temp;
-  h->hash_func = DEFAULT_HASH(temp);
-  h->comp = DEFAULT_COMP_TYPE(temp);
-  h->update = NULL;
+HTABLE *GLUE3 (hash_, prefix, _init) (int64_t expected_size) {
+    HTABLE *h = malloc (sizeof (HTABLE));
+    if (h == NULL) {
+        return NULL;
+    }
 
-  return h;
+    h->capacity = expected_size <= 16 ? 16 : GLUE3 (hash_, prefix, _roundup_pow2) (expected_size);
+    h->size = 0;
+    h->A = malloc (h->capacity * sizeof (HNODE *));
+    if (h->A == NULL) {
+        return NULL;
+    }
+    for (int64_t i = 0; i < h->capacity; i++) {
+        h->A[i] = NULL;
+    }
+    key_t temp;
+    h->hash_func = DEFAULT_HASH (temp);
+    h->comp = DEFAULT_COMP_TYPE (temp);
+    h->update = NULL;
+
+    return h;
 }
 
 /* void hash_prefix_set_hash(htable_prefix_t *h, uint64_t (*hash_func) (key_t));
@@ -115,8 +115,8 @@ HTABLE *GLUE3(hash_, prefix, _init) (int64_t expected_size) {
    must use write their own and use this routine to tell the hash table to 
    use it.    
 */
-void GLUE3(hash_, prefix, _set_hash)(HTABLE *h, uint64_t (*hash_func) (key_t)) {
-  h->hash_func = hash_func;
+void GLUE3 (hash_, prefix, _set_hash) (HTABLE *h, uint64_t (*hash_func) (key_t)) {
+    h->hash_func = hash_func;
 }
 
 /* void hash_prefix_set_comp(htable_prefix_t *h, int (*comp)(key_t, key_t));
@@ -129,8 +129,8 @@ void GLUE3(hash_, prefix, _set_hash)(HTABLE *h, uint64_t (*hash_func) (key_t)) {
    Note: if key_t is char*, strcmp is used.  
 */
 
-void GLUE3(hash_, prefix, _set_comp)(HTABLE *h, int (*comp)(key_t, key_t)) {
-  h->comp = comp;
+void GLUE3 (hash_, prefix, _set_comp) (HTABLE *h, int (*comp) (key_t, key_t)) {
+    h->comp = comp;
 }
 
 /* void hash_prefix_set_update(htable_prefix_t *h, value_t (*update)(value_t, value_t));
@@ -148,16 +148,16 @@ void GLUE3(hash_, prefix, _set_comp)(HTABLE *h, int (*comp)(key_t, key_t)) {
  
    would keep the sum the values in the hash table. 
 */
-void GLUE3(hash_, prefix, _set_update)(HTABLE *h, value_t (*update)(value_t, value_t)) {
-  h->update = update;
+void GLUE3 (hash_, prefix, _set_update) (HTABLE *h, value_t (*update) (value_t, value_t)) {
+    h->update = update;
 }
 
 /* int64_t hash_prefix_get_size(const htable_prefix_t *h);
 
    Returns the number of unique key_ts inserted into the hash table. 
 */
-int64_t GLUE3(hash_, prefix, _get_size) (const HTABLE *h) {
-  return h->size;
+int64_t GLUE3 (hash_, prefix, _get_size) (const HTABLE *h) {
+    return h->size;
 }
 
 /* int64_t hash_prefix_get_capacity(const htable_prefix_t *h);
@@ -166,8 +166,8 @@ int64_t GLUE3(hash_, prefix, _get_size) (const HTABLE *h) {
 
    Once this is 75% filled, it is automatically doubled. 
 */
-int64_t GLUE3(hash_, prefix, _get_capacity) (const HTABLE *h) {
-  return h->capacity;
+int64_t GLUE3 (hash_, prefix, _get_capacity) (const HTABLE *h) {
+    return h->capacity;
 }
 
 /* void hash_prefix_destroy(htable_prefix_t **h_ptr);
@@ -181,27 +181,27 @@ int64_t GLUE3(hash_, prefix, _get_capacity) (const HTABLE *h) {
    key_ts and/or value_ts stored in the hash table.  If these are
    pointers to structs, the caller should first iterate through the
    hash table and free them appropriately. 
-*/ 
-void GLUE3(hash_, prefix, _destroy)(HTABLE **h_ptr) {
-  HTABLE *h = *h_ptr;
-  if (h == NULL) {
-    return;
-  }
+*/
+void GLUE3 (hash_, prefix, _destroy) (HTABLE **h_ptr) {
+    HTABLE *h = *h_ptr;
+    if (h == NULL) {
+        return;
+    }
 
-  for (int64_t i = 0; i < h->capacity; i++) {
-    free(h->A[i]);
-  }
-  free(h->A);
+    for (int64_t i = 0; i < h->capacity; i++) {
+        free (h->A[i]);
+    }
+    free (h->A);
 
-  h->capacity = 0;
-  h->size = 0;
-  h->A = NULL;
-  h->hash_func = NULL;
-  h->comp = NULL;
-  h->update = NULL;
-  
-  free(h);
-  h_ptr = NULL;
+    h->capacity = 0;
+    h->size = 0;
+    h->A = NULL;
+    h->hash_func = NULL;
+    h->comp = NULL;
+    h->update = NULL;
+
+    free (h);
+    h_ptr = NULL;
 }
 
 /* int32_t hash_prefix_rehash(htable_prefix_t *h);
@@ -209,191 +209,231 @@ void GLUE3(hash_, prefix, _destroy)(HTABLE **h_ptr) {
    This routine doubles the capacity of a hash table and reinserts all
    of the entries in the new table.
 */
-int32_t GLUE3(hash_, prefix, _rehash)(HTABLE *h) {
-  if (h == NULL) {
-    return -1;
-  }
-  int64_t new_capacity = 2 * h->capacity;
-  HNODE **new_A = malloc(new_capacity * sizeof(HNODE*));
-  if (new_A == NULL) {
-    return -1;
-  }
-  for (int64_t i = 0; i < new_capacity; i++) {
-    new_A[i] = NULL;
-  }
-
-  const uint64_t mask = new_capacity - UINT64_C(1);
-
-  for (int64_t i = 0; i < h->capacity; i++) {
-    HNODE *n = h->A[i];
-    if (n == NULL) {
-      continue;
+int32_t GLUE3 (hash_, prefix, _rehash) (HTABLE *h) {
+    if (h == NULL) {
+        return -1;
     }
-    uint64_t base = n->hash & mask;
-    uint64_t step = ((n->hash / new_capacity) & mask) | UINT64_C(1);
-
-    for (uint64_t pos = base; ; pos = (pos + step) & mask) {
-      if (new_A[pos] == NULL) {
-	new_A[pos] = n;
-	break;
-      }
+    int64_t new_capacity = 2 * h->capacity;
+    HNODE **new_A = malloc (new_capacity * sizeof (HNODE *));
+    if (new_A == NULL) {
+        return -1;
     }
-  }
+    for (int64_t i = 0; i < new_capacity; i++) {
+        new_A[i] = NULL;
+    }
 
-  free(h->A);
-  h->A = new_A;
-  h->capacity = new_capacity;
-  return 0;
+    const uint64_t mask = new_capacity - UINT64_C (1);
+
+    for (int64_t i = 0; i < h->capacity; i++) {
+        HNODE *n = h->A[i];
+        if (n == NULL) {
+            continue;
+        }
+        uint64_t base = n->hash & mask;
+        uint64_t step = ((n->hash / new_capacity) & mask) | UINT64_C (1);
+
+        for (uint64_t pos = base;; pos = (pos + step) & mask) {
+            if (new_A[pos] == NULL) {
+                new_A[pos] = n;
+                break;
+            }
+        }
+    }
+
+    free (h->A);
+    h->A = new_A;
+    h->capacity = new_capacity;
+    return 0;
 }
 
-int32_t GLUE3(hash_, prefix, _put)(HTABLE *h, key_t key, value_t value) {
-  if (h == NULL || h->hash_func == NULL) {
-    return -1;
-  }
-  const uint64_t hash = h->hash_func(key);
-  const uint64_t mask = h->capacity - UINT64_C(1);
-  const uint64_t base = hash & mask;
-  const uint64_t step = ((hash / h->capacity) & mask) | UINT64_C(1);
-  
-  for (uint64_t pos = base; ; pos = (pos + step) & mask) {
-    if (h->A[pos] == NULL) {
-      HNODE *n = malloc(sizeof(HNODE));
-      if (n == NULL) {
-	return -1;
-      }
-      n->hash = hash;
-      n->key = key;
-      n->value = value;
-      h->A[pos] = n;
-      h->size++;
-      break;
-    } else if (h->A[pos]->hash == hash) {
-      if (h->comp == NULL || h->comp(key, h->A[pos]->key) == 0) {
-	h->A[pos]->value = h->update == NULL ? value : h->update(h->A[pos]->value, value);
-	break;
-      }
-    }
-  }
+/* int32_t hash_prefix_put(htable_prefix_t *h, key_t key, value_t value) 
 
-  if (h->size > LOAD_FACTOR * h->capacity) {
-    int32_t rc = GLUE3(hash_, prefix, _rehash)(h);
-    if (rc != 0) {
-      return 0;
+   Insert a key/value pair into the hash table.  
+
+   What if the key is already in the table?
+
+   1) If the user has provided a comp function (via
+      hash_prefix_set_comp) then that is used to double check on hash
+      value collisions.
+
+   2) If the user has provided an update function (via
+      hash_prefix_set_update), then the current value is combined with
+      the new value.  If not update function is avaiable, the new
+      value replaces the current value.
+
+   return value:
+     -1 => error
+     0 => ok
+*/
+
+int32_t GLUE3 (hash_, prefix, _put) (HTABLE *h, key_t key, value_t value) {
+    if (h == NULL || h->hash_func == NULL) {
+        return -1;
     }
-  }
-  return 0;
+    const uint64_t hash = h->hash_func (key);
+    const uint64_t mask = h->capacity - UINT64_C (1);
+    const uint64_t base = hash & mask;
+    const uint64_t step = ((hash / h->capacity) & mask) | UINT64_C (1);
+
+    for (uint64_t pos = base;; pos = (pos + step) & mask) {
+        if (h->A[pos] == NULL) {
+            HNODE *n = malloc (sizeof (HNODE));
+            if (n == NULL) {
+                return -1;
+            }
+            n->hash = hash;
+            n->key = key;
+            n->value = value;
+            h->A[pos] = n;
+            h->size++;
+            break;
+        }
+        else if (h->A[pos]->hash == hash) {
+            if (h->comp == NULL || h->comp (key, h->A[pos]->key) == 0) {
+                h->A[pos]->value = h->update == NULL ? value : h->update (h->A[pos]->value, value);
+                break;
+            }
+        }
+    }
+
+    if (h->size > LOAD_FACTOR * h->capacity) {
+        int32_t rc = GLUE3 (hash_, prefix, _rehash) (h);
+        if (rc != 0) {
+            return 0;
+        }
+    }
+    return 0;
 }
 
-int32_t GLUE3(hash_, prefix, _get)(HTABLE *h, key_t key, value_t *value) {
-  if (h == NULL || h->hash_func == NULL) {
-    return -1;
-  }
-  uint64_t hash = h->hash_func(key);
-  uint64_t mask = h->capacity - UINT64_C(1);
-  uint64_t base = hash & mask;
-  uint64_t step = ((hash / h->capacity) & mask) | UINT64_C(1);
-  
-  for (uint64_t pos = base; ; pos = (pos + step) & mask) {
-    if (h->A[pos] == NULL) {
-      return 0;
-    } else {
-      if (h->A[pos]->hash == hash) {
-	if (h->comp == NULL || h->comp(key, h->A[pos]->key) == 0) {
-	  if (value != NULL) {
-	    *value = h->A[pos]->value;
-	  }
-	  return 1;
-	}
-      }
+/* int32_T hash_prefix_get(htable_prefix_t *h, key_t key, value_t *value) 
+
+   Recovers a value from a hash table.  *value can be NULL if the user 
+   only wants to know if the key is in the table.  
+
+   Return Value:
+     -1 => an error
+     0 => key not found
+     1 => key found, *value set to the corresponding value.
+*/
+
+int32_t GLUE3 (hash_, prefix, _get) (HTABLE *h, key_t key, value_t *value) {
+    if (h == NULL || h->hash_func == NULL) {
+        return -1;
     }
-  }
+    uint64_t hash = h->hash_func (key);
+    uint64_t mask = h->capacity - UINT64_C (1);
+    uint64_t base = hash & mask;
+    uint64_t step = ((hash / h->capacity) & mask) | UINT64_C (1);
+
+    for (uint64_t pos = base;; pos = (pos + step) & mask) {
+        if (h->A[pos] == NULL) {
+            return 0;
+        }
+        else {
+            if (h->A[pos]->hash == hash) {
+                if (h->comp == NULL || h->comp (key, h->A[pos]->key) == 0) {
+                    if (value != NULL) {
+                        *value = h->A[pos]->value;
+                    }
+                    return 1;
+                }
+            }
+        }
+    }
 }
 
-int32_t GLUE3(hash_, prefix, _remove)(HTABLE *h, key_t key, value_t *value) {
-  if (h == NULL || h->hash_func == NULL) {
-    return -1;
-  }
-  uint64_t hash = h->hash_func(key);
-  uint64_t mask = h->capacity - UINT64_C(1);
-  uint64_t base = hash & mask;
-  uint64_t step = ((hash / h->capacity) & mask) | UINT64_C(1);
-  
-  for (uint64_t pos = base; ; pos = (pos + step) & mask) {
-    if (h->A[pos] == NULL) {
-      return 0;
-    } else {
-      if (h->A[pos]->hash == hash) {
-	if (h->comp == NULL || h->comp(key, h->A[pos]->key) == 0) {
-	  if (value != NULL) {
-	    *value = h->A[pos]->value;
-	  }
-	  free(h->A[pos]);
-	  h->A[pos] = NULL;
-	  h->size--;
-	  return 1;
-	}
-      }
-    }
-  }
-}
+/* hash_prefix_remove(htable_prefix_t *h, key_t key, value_t *value) 
 
-int32_t GLUE3(hash_, prefix, _next) (HITER **iter_ptr, key_t *key, value_t *value) {
-  HITER *iter = *iter_ptr;
-  const HTABLE *h = iter->h;
-  uint64_t curr = iter->curr;
-
-  while (1) {
-    if (curr == h->capacity) {
-      iter->h = NULL;
-      iter->curr = -1;
-      free(iter);
-      *iter_ptr = NULL;
-      return 1;
-    }
-    
-    if (h->A[curr] != NULL) {
-      if (key != NULL) {
-	*key = h->A[curr]->key;
-      }
-      if (value != NULL) {
-	*value = h->A[curr]->value;
-      }
-      curr++;
-      iter->curr = curr;
-      return 0;
-    }
-    
-    curr++;
-  }
-}
-
-int32_t GLUE3(hash_, prefix, _first) (const HTABLE *h, HITER **iter_ptr, key_t *key, value_t *value) {
-  HITER *iter = malloc(sizeof(HITER));
-  iter->h = h;
-  iter->curr = 0;
-  *iter_ptr = iter;
-
-  return GLUE3(hash_, prefix, _next)(iter_ptr, key, value);
-}
-
-void GLUE3(hash_, prefix, _apply) (HTABLE *h, value_t (*f)(key_t, value_t)) {
-  for (int64_t idx = 0; idx < h->capacity; idx++) {
-    HNODE *a = h->A[idx];
-    if (a != NULL) {
-      a->value = f(a->key, a->value);
-    }
-  }
-}
-
-void GLUE3(hash_, prefix, _apply_r) (HTABLE *h, value_t (*f)(key_t, value_t, void *), void *arg) {
-    for (int64_t idx = 0; idx < h->capacity; idx++) {
-    HNODE *a = h->A[idx];
-    if (a != NULL) {
-      a->value = f(a->key, a->value, arg);
-    }
-  }
-}
    
+*/
+
+int32_t GLUE3 (hash_, prefix, _remove) (HTABLE *h, key_t key, value_t *value) {
+    if (h == NULL || h->hash_func == NULL) {
+        return -1;
+    }
+    uint64_t hash = h->hash_func (key);
+    uint64_t mask = h->capacity - UINT64_C (1);
+    uint64_t base = hash & mask;
+    uint64_t step = ((hash / h->capacity) & mask) | UINT64_C (1);
+
+    for (uint64_t pos = base;; pos = (pos + step) & mask) {
+        if (h->A[pos] == NULL) {
+            return 0;
+        }
+        else {
+            if (h->A[pos]->hash == hash) {
+                if (h->comp == NULL || h->comp (key, h->A[pos]->key) == 0) {
+                    if (value != NULL) {
+                        *value = h->A[pos]->value;
+                    }
+                    free (h->A[pos]);
+                    h->A[pos] = NULL;
+                    h->size--;
+                    return 1;
+                }
+            }
+        }
+    }
+}
+
+int32_t GLUE3 (hash_, prefix, _next) (HITER **iter_ptr, key_t *key, value_t *value) {
+    HITER *iter = *iter_ptr;
+    const HTABLE *h = iter->h;
+    uint64_t curr = iter->curr;
+
+    while (1) {
+        if (curr == h->capacity) {
+            iter->h = NULL;
+            iter->curr = -1;
+            free (iter);
+            *iter_ptr = NULL;
+            return 1;
+        }
+
+        if (h->A[curr] != NULL) {
+            if (key != NULL) {
+                *key = h->A[curr]->key;
+            }
+            if (value != NULL) {
+                *value = h->A[curr]->value;
+            }
+            curr++;
+            iter->curr = curr;
+            return 0;
+        }
+
+        curr++;
+    }
+}
+
+int32_t GLUE3 (hash_, prefix, _first) (const HTABLE *h, HITER **iter_ptr, key_t *key,
+                                       value_t *value) {
+    HITER *iter = malloc (sizeof (HITER));
+    iter->h = h;
+    iter->curr = 0;
+    *iter_ptr = iter;
+
+    return GLUE3 (hash_, prefix, _next) (iter_ptr, key, value);
+}
+
+void GLUE3 (hash_, prefix, _apply) (HTABLE *h, value_t (*f) (key_t, value_t)) {
+    for (int64_t idx = 0; idx < h->capacity; idx++) {
+        HNODE *a = h->A[idx];
+        if (a != NULL) {
+            a->value = f (a->key, a->value);
+        }
+    }
+}
+
+void GLUE3 (hash_, prefix, _apply_r) (HTABLE *h, value_t (*f) (key_t, value_t, void *), void *arg) {
+    for (int64_t idx = 0; idx < h->capacity; idx++) {
+        HNODE *a = h->A[idx];
+        if (a != NULL) {
+            a->value = f (a->key, a->value, arg);
+        }
+    }
+}
+
 #undef HNODE
 #undef HTABLE
 #undef GLUE3
