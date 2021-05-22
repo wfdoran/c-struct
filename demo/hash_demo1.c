6a350a9594ef8d921cabe3856084e0c044c1200f
William Doran <wfdoran4@yahoo.com>
2021-05-22 10:16:01 -0700

    Improved hash_demo1.

--- demo/hash_demo1.c
+++ demo/hash_demo1.c
@@ -11,53 +11,56 @@
 #undef key_t
 
 int main() {
+  /* Example of using hash.h */
   char* a = "Bill Doran";
   uint64_t (*my_hash)(char*) = DEFAULT_HASH(a);
   uint64_t a_hash = my_hash(a);
-  printf("%016lx\n", a_hash);
+  printf("hash of \"Bill Doran\" = %016lx\n", a_hash);
+  printf("\n");
 
+  /* Create a hash table which gives us inverse 
+     square root for integers. */
   htable_int_t *h = hash_int_init(0);
-  hash_int_put(h, 0, 0);
 
   for (int i = 1; i <= 100; i++) {
     int rc = hash_int_put(h, i*i, i);
     assert(rc == 0);
   }
-  hash_int_put(h, 0, -5);
+  printf("hash table size = %ld  capacity = %ld\n",
+	 hash_int_get_size(h), hash_int_get_capacity(h));
+  printf("\n");
 
-  {
+  /* Try getting a few values. */
+  for (int v = 1; v <= 16; v++) {
     int sqrt;
-    int rc;
-
-    sqrt = 0;
-    rc = hash_int_get(h, 16, &sqrt);
-    printf("%d %d\n", rc, sqrt);
-
-    sqrt = 0;
-    rc = hash_int_get(h, 17, &sqrt);
-    printf("%d %d\n", rc, sqrt);
-
-    sqrt = 0;
-    rc = hash_int_get(h, 0, &sqrt);
-    printf("%d %d\n", rc, sqrt);    
+    int rc = hash_int_get(h, v, &sqrt);
+    if (rc == 1) {
+      printf("sqrt(%d) = %d\n", v, sqrt);
+    } else {
+      printf("sqrt(%d) is not an integer\n", v);
+    }
   }
+  printf("\n");
   
-  printf("%ld \n", h->size);
-
-
+  /* Remove a few values */
   {
     for (int i = 100; i <= 200; i++) {
-      hash_int_remove(h, i, NULL);
+      int rc = hash_int_remove(h, i, NULL);
+      if (rc == 1) {
+	printf("Removed %d\n", i);
+      }
     }
   }
 
-  printf("number of entries %ld of %ld\n", hash_int_get_size(h), hash_int_get_capacity(h));
- 
+  printf("hash table size = %ld  capacity = %ld\n",
+	 hash_int_get_size(h), hash_int_get_capacity(h));
+  printf("\n");
+  
   hiter_int_t *iter;
   int key;
   int value;
   for (hash_int_first(h, &iter, &key, &value); iter != NULL; hash_int_next(&iter, &key, &value)) {
-    printf("%8d %8d\n", key, value); 
+    printf("sqrt(%8d) =  %8d\n", key, value); 
   }
 
   hash_int_destroy(&h);
