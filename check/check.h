1405b6df582dc246c3ad3cb9318afa1571379926
William Doran <wfdoran4@yahoo.com>
2024-09-11 13:13:34 -0700

    Added a simple RNG for repeatability.

--- check/check.h
+++ check/check.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define START_TEST(a)				\
   int a() {					\
@@ -17,3 +18,25 @@
   return 1;					\
   } \
   } while(0)
+
+
+uint32_t _rng_state = 0;
+
+// https://en.wikipedia.org/wiki/Linear_congruential_generator
+uint32_t get_rand() {
+  uint32_t a = 1664525;
+  uint32_t c = 1013904223;
+
+  _rng_state = (a * _rng_state) + c;
+  uint32_t rv = (a >> 16);
+
+  _rng_state = (a * _rng_state) + c;
+  rv = (_rng_state & 0xffff) ^ rv;
+
+  return rv;
+}
+
+void seed_rand(uint32_t seed) {
+  _rng_state = seed;
+}
+
