cb02bee80463fa225062ad633981f5a208a3c520
William Doran <wfdoran4@yahoo.com>
2024-09-19 11:39:15 -0700

    Added linked_list_test2 testing llink_xxx_add_start and llink_xxx_remove_tail.

--- check/linked_list_tests.c
+++ check/linked_list_tests.c
@@ -16,3 +16,23 @@ llist_int_destroy(&a);
 CHECK(a == NULL);
 
 END_TEST
+
+START_TEST(linked_list_test2)
+
+llist_int_t *a = llist_int_init();
+CHECK(a != NULL);
+
+int n = 10;
+for (int i = 0; i < n; i++) {
+  llist_int_add_start(a, i);
+}
+
+for (int i = 0; i < n; i++) {
+  CHECK(llist_int_remove_end(a) == i);
+}
+CHECK(llist_int_remove_end(a) == -1);
+
+llist_int_destroy(&a);
+CHECK(a == NULL);
+
+END_TEST
