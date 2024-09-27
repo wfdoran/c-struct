1e095a15e042b70ebf6a2549a300b50139c833d6
William Doran <wfdoran4@yahoo.com>
2024-09-27 10:46:29 -0700

    Added linked_list_test[3-5] testing add_start, add_end, remove_start, remove_end.

--- check/linked_list_tests.c
+++ check/linked_list_tests.c
@@ -36,3 +36,62 @@ llist_int_destroy(&a);
 CHECK(a == NULL);
 
 END_TEST
+
+START_TEST(linked_list_test3)
+
+llist_int_t *a = llist_int_init();
+CHECK(a != NULL);
+
+int n = 10;
+for (int i = 0; i < n; i++) {
+  llist_int_add_end(a, i);
+}
+
+for (int i = 0; i < n; i++) {
+  CHECK(llist_int_remove_start(a) == i);
+}
+CHECK(llist_int_remove_end(a) == -1);
+
+llist_int_destroy(&a);
+CHECK(a == NULL);
+
+END_TEST
+
+START_TEST(linked_list_test4)
+
+llist_int_t *a = llist_int_init();
+CHECK(a != NULL);
+
+int n = 10;
+for (int i = 0; i < n; i++) {
+  llist_int_add_start(a, i);
+}
+
+for (int i = 0; i < n; i++) {
+  CHECK(llist_int_remove_start(a) == n - i - 1);
+}
+CHECK(llist_int_remove_end(a) == -1);
+
+llist_int_destroy(&a);
+CHECK(a == NULL);
+
+END_TEST
+START_TEST(linked_list_test5)
+
+llist_int_t *a = llist_int_init();
+CHECK(a != NULL);
+
+int n = 10;
+for (int i = 0; i < n; i++) {
+  llist_int_add_end(a, i);
+}
+
+for (int i = 0; i < n; i++) {
+  CHECK(llist_int_remove_end(a) == n - i - 1);
+}
+CHECK(llist_int_remove_end(a) == -1);
+
+llist_int_destroy(&a);
+CHECK(a == NULL);
+
+END_TEST
