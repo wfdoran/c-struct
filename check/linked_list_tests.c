4edc9a53976ff26c4c5972577f46d22e5c7279f4
William Doran <wfdoran4@yahoo.com>
2024-10-01 15:52:27 -0700

    Added linked_list_test6, walking a linked list forward.

--- check/linked_list_tests.c
+++ check/linked_list_tests.c
@@ -76,6 +76,7 @@ llist_int_destroy(&a);
 CHECK(a == NULL);
 
 END_TEST
+
 START_TEST(linked_list_test5)
 
 llist_int_t *a = llist_int_init();
@@ -90,6 +91,29 @@ for (int i = 0; i < n; i++) {
   CHECK(llist_int_remove_end(a) == n - i - 1);
 }
 CHECK(llist_int_remove_end(a) == -1);
+
+llist_int_destroy(&a);
+CHECK(a == NULL);
+
+END_TEST
+
+START_TEST(linked_list_test6)
+
+llist_int_t *a = llist_int_init();
+CHECK(a != NULL);
+
+int n = 10;
+for (int i = 0; i < 10; i++) {
+  llist_int_add_end(a, i);
+}
+
+int y = 0;
+lnode_int_t *nn = NULL;
+for (int x = llist_int_walk_init_start(a, &nn); x != -1; x = llist_int_walk_forward(&nn)) {
+  CHECK(x == y);
+  y++;
+}
+CHECK(y == n);
 
 llist_int_destroy(&a);
 CHECK(a == NULL);
