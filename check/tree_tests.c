e23db6835475e59ca5876f706de9234af3662ad7
William Doran <wfdoran4@yahoo.com>
2024-10-04 13:50:32 -0700

    Added tree_test2 testing tree_XXX_insert.

--- check/tree_tests.c
+++ check/tree_tests.c
@@ -16,3 +16,23 @@ tree_int_destroy(&a);
 CHECK(a == NULL);
 
 END_TEST
+
+
+START_TEST(tree_test2)
+
+tree_int_t *a = tree_int_init();
+CHECK(a != NULL);
+
+int n = 10;
+for (int i = 0; i < n; i++) {
+  tree_int_insert(a, i, NULL);
+}
+
+CHECK(tree_int_size(a) == n);
+
+tree_int_destroy(&a);
+CHECK(a == NULL);
+
+END_TEST
+
+
