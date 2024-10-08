7c727287a516ff5b59609f4fa091e47e53dc7948
William Doran <wfdoran4@yahoo.com>
2024-10-08 13:00:06 -0700

    Added tree_test4.c testing tree_XXX_delete_max

--- check/tree_tests.c
+++ check/tree_tests.c
@@ -76,3 +76,38 @@ tree_int_destroy(&a);
 CHECK(a == NULL);
 
 END_TEST
+
+
+START_TEST(tree_test4)
+
+seed_rand(445);
+
+tree_int_t *a = tree_int_init();
+CHECK(a != NULL);
+
+int n = 10;
+for (int i = 0; i < n; i++) {
+  int key = get_rand() & 0xffff;
+  tree_int_insert(a, key, NULL);
+}
+
+int prev = 0;
+while (true) {
+  key_int_value_t x = tree_int_delete_min(a);
+  if (!x.found) {
+    break;
+  }
+
+  int key = x.key;
+  CHECK(key >= prev);
+  prev = key;
+}
+
+CHECK(tree_int_size(a) == 0);
+
+tree_int_destroy(&a);
+CHECK(a == NULL);
+
+END_TEST
+
+
