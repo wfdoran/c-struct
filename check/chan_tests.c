b17b00d8e4346bc59b63a5ff8896e4587cbeb164
William Doran <wfdoran4@yahoo.com>
2024-10-10 18:16:11 -0700

    Added chan_test2.

--- check/chan_tests.c
+++ check/chan_tests.c
@@ -18,3 +18,28 @@ for (int chan_size = 1; chan_size <= 100; chan_size *= 10) {
 }
 
 END_TEST
+
+START_TEST(chan_test2)
+
+int n = 10;
+
+chan_int_t *a = chan_int_init(n);
+CHECK(a != NULL);
+
+for (int i = 0; i < n; i++) {
+  int32_t rc = chan_int_send(a, i);
+  CHECK(rc == CHAN_SUCCESS);
+}
+
+for (int i = 0; i < n; i++) {
+  int value;
+  int32_t rc = chan_int_recv(a, &value);
+  CHECK(rc == CHAN_SUCCESS);
+  CHECK(value == i);
+}
+
+chan_int_destroy(&a);
+CHECK(a == NULL);
+
+END_TEST
+
