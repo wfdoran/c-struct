713ffab200d301dadfe5729daa915624225e10c2
William Doran <wfdoran4@yahoo.com>
2025-03-22 16:47:54 -0700

    Added interval_log and interval_exp.

--- bin/interval.c
+++ bin/interval.c
@@ -129,6 +129,52 @@ interval_div(interval_t a, interval_t b) {
   return rv;  
 }
 
+interval_t
+interval_exp(interval_t a) {
+  if (!a.valid) {
+    interval_t bad = {.lo = 0, .hi = 0, .valid = false};
+    return bad;
+  }
+
+  interval_t rv;
+  int save = fegetround();
+
+  fesetround(FE_UPWARD);
+  rv.hi = exp(a.hi);
+
+  fesetround(FE_DOWNWARD);
+  rv.lo = exp(a.lo);
+
+  fesetround(save);
+
+  rv.valid = true;
+  return rv;
+}
+
+interval_t
+interval_log(interval_t a) {
+  if (!a.valid || a.lo <= 0.0) {
+    interval_t bad = {.lo = 0.0, .hi = 0.0, .valid = false};
+    return bad;
+  }
+
+  interval_t rv;
+  int save = fegetround();
+
+  fesetround(FE_UPWARD);
+  rv.hi = log(a.hi);
+
+  fesetround(FE_DOWNWARD);
+  rv.lo = log(a.lo);
+
+  fesetround(save);
+
+  rv.valid = true;
+  return rv;
+}
+
+  
+    
 
 void
 interval_print(interval_t a) {
@@ -150,6 +196,26 @@ int main(void) {
   interval_print(e);
   interval_t f = interval_div(a,b);
   interval_print(f);
+
+  interval_t x = interval_from_double(10.0);
+  interval_t y = interval_from_double(100.0);
+  interval_t z = interval_from_double(1000.0);
+
+  interval_t w1 = interval_add(x, interval_add(y, z));
+  interval_t w2 = interval_add(interval_add(x, y), z);
+  interval_t w3 = interval_add(interval_add(y, x), z);
+  interval_print(w1);
+  interval_print(w2);
+  interval_print(w3);
+
+
+  {
+    interval_t g = interval_from_double(3.3838);
+    for (int i = 0; i < 10; i++) {
+      g = interval_log(interval_exp(g));
+      interval_print(g);
+    }
+  }
 
   return 0;
 }
