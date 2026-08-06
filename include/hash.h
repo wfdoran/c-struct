e5833c6b8556bd129b939444b535ca04b8c4fa38
William Doran <wfdoran4@yahoo.com>
2026-08-05 17:41:38 -0700

    Fixed some bugs in hash.h

--- include/hash.h
+++ include/hash.h
@@ -5,12 +5,12 @@
 #include <string.h>
 #include <stdlib.h>
 
-uint64_t hash_uint64_t(uint64_t x) {
+static inline uint64_t hash_uint64_t(uint64_t x) {
   return x;
 }
 
 #define GENERIC_HASH(t)	    \
-  uint64_t hash_##t (t x) { \
+  static inline uint64_t hash_##t (t x) { \
     return (uint64_t) x;    \
   }
 
@@ -22,11 +22,9 @@ GENERIC_HASH(uint16_t)
 GENERIC_HASH(int8_t)
 GENERIC_HASH(uint8_t)
 GENERIC_HASH(char)
-GENERIC_HASH(double)
-GENERIC_HASH(float)
 
 
-uint64_t hash_str(char *s) {
+static inline uint64_t hash_str(const char *s) {
   uint64_t rv = UINT64_C(0x5555555555555555);
   uint64_t mult = UINT64_C(6364136223846793005);
 
@@ -37,19 +35,33 @@ uint64_t hash_str(char *s) {
   return rv;
 }
 
+/* ToDo: clean with -0.0, NaN, and inf better */
+static inline uint64_t hash_double(double x) {
+  uint64_t bits = 0;
+  memcpy(&bits, &x, sizeof(double));
+  return bits;
+}
+
+static inline uint64_t hash_float(float x) {
+  uint64_t bits = 0;
+  memcpy(&bits, &x, sizeof(float));
+  return bits;
+}
+
 #define DEFAULT_HASH(x) _Generic((x),		\
     uint64_t: &hash_uint64_t, \
     int64_t: &hash_int64_t, \
     uint32_t: &hash_uint32_t, \
     int32_t: &hash_int32_t, \
     uint16_t: &hash_uint16_t, \
-    int16_t: &hash_int64_t, \
+    int16_t: &hash_int16_t, \
     uint8_t: &hash_uint8_t, \
     int8_t: &hash_int8_t,\
     char: &hash_char, \
     double: &hash_double, \
     float: &hash_float, \
     char*: &hash_str, \
+    const char*: &hash_str, \
     default: NULL)
 
 #undef GENERIC_HASH
